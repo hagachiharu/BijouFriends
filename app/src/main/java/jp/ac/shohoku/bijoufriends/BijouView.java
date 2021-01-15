81f89231c88fbadcba0c4302fe38930c3aa5d741
s20b708 <s20b708@shohoku.ac.jp>
2021-01-15 11:23:42 +0900

    メゾット

--- app/src/main/java/jp/ac/shohoku/bijoufriends/BijouView.java
+++ app/src/main/java/jp/ac/shohoku/bijoufriends/BijouView.java
@@ -53,42 +53,50 @@ public class BijouView extends View {
         Paint p = new Paint();
 
         if(state == FIRST) {  // 状態1の場合の描画
-            Resources rs = this.getContext().getResources(); //リソースを取得
-            Bitmap bijyou = BitmapFactory.decodeResource(rs, R.drawable.bijyou); //画像を取得
-            w1 = this.getWidth();  //this=bijyouview
-            h1 = this.getHeight();
-            w = bijyou.getWidth();
-            h = bijyou.getHeight();
-            canvas.drawBitmap(bijyou, w1/3-w/3, h1/3-h/3, p); //画像の左上を Canvasの(0,0)に合わせて表示する
-
-            Resources rt = this.getContext().getResources(); //リソースを取得
-            Bitmap start = BitmapFactory.decodeResource(rt, R.drawable.start); //画像を取得
-            w2 = this.getWidth();
-            h2 = this.getHeight();
-            w3 = start.getWidth();
-            h3 = start.getHeight();
-            canvas.drawBitmap(start, w1/2-w/2, h1/2-h/2, p); //画像の左上を Canvasの(0,0)に合わせて表示する
-
-
-
-            Resources ru = this.getContext().getResources(); //リソースを取得
-            Bitmap rabbit = BitmapFactory.decodeResource(ru, R.drawable.rabbit); //画像を取得
-            w4 = this.getWidth();
-            h4 = this.getHeight();
-            w5 = rabbit.getWidth();
-            h5 = rabbit.getHeight();
-            canvas.drawBitmap(rabbit, 1000, 1700, p); //画像の左上を Canvasの(0,0)に合わせて表示する
-
+            drawLv1(canvas, p);
         }
         else if (state == SECOND) {  // 状態2の場合の描画
-
+            drawLv2();
         }
         else if (state == THIRD) {  // 状態3の場合の描画
-
+            drawLv3();
         }
         else {
             Log.d("error", "never come here");
         }
+    }
+
+    private void drawLv1(Canvas canvas, Paint p) {
+        Resources rs = this.getContext().getResources(); //リソースを取得
+        Bitmap bijyou = BitmapFactory.decodeResource(rs, R.drawable.bijyou); //画像を取得
+        w1 = this.getWidth();  //this=bijyouview
+        h1 = this.getHeight();
+        w = bijyou.getWidth();
+        h = bijyou.getHeight();
+        canvas.drawBitmap(bijyou, w1/3-w/3, h1/3-h/3, p); //画像の左上を Canvasの(0,0)に合わせて表示する
+
+        Resources rt = this.getContext().getResources(); //リソースを取得
+        Bitmap start = BitmapFactory.decodeResource(rt, R.drawable.start); //画像を取得
+        w2 = this.getWidth();
+        h2 = this.getHeight();
+        w3 = start.getWidth();
+        h3 = start.getHeight();
+        canvas.drawBitmap(start, w1/2-w/2, h1/2-h/2, p); //画像の左上を Canvasの(0,0)に合わせて表示する
+
+
+        Resources ru = this.getContext().getResources(); //リソースを取得
+        Bitmap rabbit = BitmapFactory.decodeResource(ru, R.drawable.rabbit); //画像を取得
+        w4 = this.getWidth();
+        h4 = this.getHeight();
+        w5 = rabbit.getWidth();
+        h5 = rabbit.getHeight();
+        canvas.drawBitmap(rabbit, 1000, 1700, p); //画像の左上を Canvasの(0,0)に合わせて表示する
+    }
+
+    private void drawLv3() {
+    }
+
+    private void drawLv2() {
     }
 
 
@@ -103,10 +111,12 @@ public class BijouView extends View {
             }
         }
         else if(state == SECOND){  // 状態2だったら状態3へ
-            state = THIRD;
+            touchLv2();
+            //state = THIRD;
         }
         else if(state == THIRD){  // 状態3だったら状態1へ
-            state = FIRST;
+            //state = FIRST;
+            touchLv3();
         }
         else{  // それ以外だったらエラーを吐き出す
             Log.d("error", "never come here");
@@ -116,7 +126,15 @@ public class BijouView extends View {
         return super.onTouchEvent(event);
     }
 
-    
+    //Lv3でタッチされた時の処理
+    private void touchLv3() {
+    }
+
+
+    private void touchLv2() {
+    }
+
+
     public BijouView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
